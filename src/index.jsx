bfa0bb97e6bcb807d46cde98e8d18cc8053a4cc0
Iva Havranová <havranova.i@seznam.cz>
2023-05-20 15:34:08 +0200

    in progress

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 
@@ -9,13 +9,22 @@ const MenuItem = (props) => {
     </a>
   );
 };
-
+// Chceme, aby komponenta MenuItem přijímala prop s názvem onSelect. Ta bude očekávat callback, který se zavolá, když uživatel vybere danou položku. Předejte tedy callback handleSelectItem všem komponentám MenuItem jako prop s názvem onSelect.
 const App = () => {
+  const handleSelectItem = () => {
+    setMenuOpened(!menuOpened);
+  };
+  const [menuOpened, setMenuOpened] = useState(true);
   return (
     <>
       <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
+        <div className={menuOpened ? 'menu menu--closed' : 'menu'}>
+          <button
+            className="menu__btn"
+            onClick={() => {
+              setMenuOpened(!menuOpened);
+            }}
+          ></button>
           <div className="menu__items">
             <MenuItem text="Domů" />
             <MenuItem text="Naše nabídka" />
